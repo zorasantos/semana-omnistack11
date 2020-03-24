3e671804ed6b3c870c874ec7ed7c08253be396aa
Zoranildo Santos <zoranildo22@gmail.com>
2020-03-24 18:21:35 -0300

    Backend finalizado

--- backend/src/controllers/incidentController.js
+++ backend/src/controllers/incidentController.js
@@ -2,7 +2,18 @@ const connection = require('../database/connection')
 
 module.exports = {
   async index(req, res) {
-    const incidents = await connection('incidents').select('*')
+    const { page = 1 } = req.query
+
+    const [count] = await connection('incidents').count()
+
+    console.log(count)
+
+    const incidents = await connection('incidents')
+      .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+      .limit(5)
+      .offset((page -1) * 5)
+      .select(['incidents.*', 'ongs.name', 'ongs.email', 'ongs.whatsapp', 'ongs.city', 'ongs.uf'])
+      res.header('X-Total-Count', count['count(*)'])
 
     return res.json(incidents)
   },
@@ -18,5 +29,23 @@ module.exports = {
     })
 
     return res.json({ id })
+  },
+
+  async delete(req, res) {
+    const { id } = req.params
+    const ong_id = req.headers.authorization
+
+    const incidents = await connection('incidents')
+      .where('id', id)
+      .select('ong_id')
+      .first()
+
+      if(incidents.ong_id !== ong_id) {
+        return res.status(401).json({ error: 'Operation not permitted.' })
+      }
+
+      await connection('incidents').where('id', id).delete()
+
+      return res.status(204).send()
   }
 }
