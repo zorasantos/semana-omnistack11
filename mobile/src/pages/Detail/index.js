bfa39964523b930eb3f2de7a27e5b5d69315eae8
Zoranildo Santos <zoranildo22@gmail.com>
2020-03-26 19:28:17 -0300

    First commit

---
+++ mobile/src/pages/Detail/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Detail() {
+  return (
+    
+    <View>
+      <Text>
+        Puta que pariu
+      </Text>
+    </View>
+  )
+}
